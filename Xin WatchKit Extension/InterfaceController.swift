c70717b1e60cd31becbf7a4aa46b8ddf41ccf3f6
al03 <maokaiyin@gmail.com>
2016-12-18 00:13:37 +0800

    Initial Commit

---
+++ Xin WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Xin WatchKit Extension
+//
+//  Created by Albert Mao on 12/18/16.
+//  Copyright © 2016 Albert Mao. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
